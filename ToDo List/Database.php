e38141eb2f957ccd3c6e68e159e33e2b5e802724
Reyam <reyamsalameh@gmail.com>
2026-01-03 19:23:29 +0300

    Delete ToDo List directory

--- ToDo List/Database.php
+++
@@ -1,7 +0,0 @@
-<?php
-	$connect = mysqli_connect("localhost", "root", "", "todo_list");
-
-	if ($connect == false) {
-		die("Connection failed");
-	}
-?>
--- ToDo List/Delete.php
+++
@@ -1,9 +0,0 @@
-<?php
-	require "Database.php";
-
-	$id = $_GET['id'];
-	mysqli_query($connect, "DELETE FROM tasks WHERE id=$id");
-
-	header("Location: List.php");
-	exit;
-?>
