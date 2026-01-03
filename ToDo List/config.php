14b5ac239afcc7f278ef56db06852d73441610d2
Reyam <reyamsalameh@gmail.com>
2026-01-03 21:10:44 +0300

    config.php

--- ToDo List/config.php
+++ ToDo List/config.php
@@ -12,9 +12,9 @@ if (!isset($_COOKIE['font'])) {
 $theme = "light";
 $font  = "large"; 
 
-//if (isset($_COOKIE['theme']) && $_COOKIE['theme'] == "dark") {
-//    $theme = "dark";
-//}
+if (isset($_COOKIE['theme']) && $_COOKIE['theme'] == "dark") {
+    $theme = "dark";
+}
 
 if (isset($_COOKIE['font'])) {
     $font = $_COOKIE['font'];
